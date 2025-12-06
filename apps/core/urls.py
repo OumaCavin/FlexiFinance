5ddf2ad34415e06d51b86ec0b796df2c1943ca06
OumaCavin <cavin.otieno012@gmail.com>
2025-12-06 20:21:34 +0800

    fix: remove app_name namespace from core urls and update templates to use 'home' instead of 'core:home'

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -6,8 +6,6 @@ URL patterns for main website pages
 from django.urls import path
 from . import views
 
-app_name = 'core'
-
 urlpatterns = [
     # Main website pages
     path('', views.HomeView.as_view(), name='home'),
