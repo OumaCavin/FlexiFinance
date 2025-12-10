a02c3905f1aaf72171406f44f3ae834f9540f2d7
OumaCavin <cavin.otieno012@gmail.com>
2025-12-10 18:34:56 +0800

    Sync with matrix message 343281649582246

--- apps/payments/urls.py
+++ apps/payments/urls.py
@@ -4,8 +4,6 @@ URL patterns for Payments app
 from django.urls import path, include
 from apps.payments import api_urls
 
-app_name = 'payments_main'
-
 urlpatterns = [
     # API endpoints
     path('', include(api_urls)),
