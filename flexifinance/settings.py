84dd0192d1f8700241aa7774a158c37729f2a78c
OumaCavin <cavin.otieno012@gmail.com>
2025-12-12 02:17:31 +0800

    fix: correct email configuration for Mailpit integration
    - Update EMAIL_PORT from 2526 to 1025 (standard Mailpit SMTP port)
    - Enable EMAIL_USE_TLS to True for secure email communication
    - This resolves ConnectionRefusedError during user signup process
    - Email verification now works properly with Mailpit

--- flexifinance/settings.py
+++ flexifinance/settings.py
@@ -292,8 +292,8 @@ CORS_ALLOW_ALL_ORIGINS = False  # Set to False for production
 # To use Mailpit SMTP, uncomment the following:
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'localhost'
-EMAIL_PORT = 2526
-EMAIL_USE_TLS = False
+EMAIL_PORT = 1025
+EMAIL_USE_TLS = True
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_TIMEOUT = 30
